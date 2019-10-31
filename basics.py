531e59d33da36977e94eda23974ceadd817e2a59
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2019-11-01 01:07:59 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,109 @@
+#Remove hashes from the block of code that you want to run
+
+#import math
+#print("\n\n___________________________________")
+#x1=int(input("x1 : "))
+#x2=int(input("x2 : "))
+#y1=int(input("y1 : "))
+#y2=int(input("y2 : "))
+#output = math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
+#print("Distance = "+str(output))
+#print("\n\n___________________________________")
+
+
+
+
+#print("\n\n___________________________________")
+#string = "\n\n\nfirst program"
+#string += " in python\n\n\n"
+#print(string)
+#print("___________________________________\n\n")
+
+
+
+
+#print("\n\n___________________________________\n\n")
+#x = int(input("Enter a number : "))
+#y = int(input("\nEnter another number : "))
+#output = "\n\nSum is "
+#output += str(x+y)
+#print(output)
+#print("\n\n___________________________________\n\n")
+
+
+
+
+#print("\n\n___________________________________\n\n")
+#x = int(input("Enter a number : "))
+#y = int(input("Enter another number : "))
+#output = "Difference is "
+#if (x-y)<0
+#    output += str(y-x)    
+#else
+#    output += str(x-y)
+#end
+#
+#print(output)
+#print("\n\n___________________________________\n\n")
+
+
+
+
+#print("\n\n___________________________________\n\n")
+#x = int(input("Enter a number : "))
+#y = int(input("Enter another number : "))
+#output = "Multiplication is "
+#output += str(x*y)
+#print(output)
+#print("\n\n___________________________________\n\n")
+
+
+
+
+#print("\n\n___________________________________\n\n")
+#x = int(input("Enter a dividend : "))
+#y = int(input("Enter divisor : "))
+#div = x // y
+#rem = (x / y) - (x // y)
+#output1 = "Quotient is "
+#output1 += str(div)
+#output2 = "Remainder is "
+#output2 += str(rem)
+#print(output1)
+#print(output2)
+#print("\n\n___________________________________\n\n")
+
+
+
+
+#print("\n\n___________________________________\n\n")
+#def fact(a):
+#    if(a==0):
+#        return 1
+#    else:
+#        return a * fact(a-1)
+#    
+#x = int(input('Enter a number : '))
+#out = fact(x)
+#print('Factorial = ' + str(out))
+#print("\n\n___________________________________\n\n")
+
+
+
+
+#print("\n\n___________________________________\n\n")
+#def fib(init,finl):
+#    if(finl==0):        
+#        return 1
+#    else:
+#        finl-=1  
+#        init+=init+1        
+#        return init
+#        fib(init,finl)
+#   #x = int(input("Enter initial number : "))
+#x = int(input("Enter the number of terms : "))
+#output = "Sequnce upto "
+#output += str(x)
+#output += " numbers is :-"
+#fib(1,x)
+#print("\n\n___________________________________\n\n")
